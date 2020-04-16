4e76294cff58735281bfdb2b0662c74de2a0bfbe
Andrea Jansen <andyjan2317@gmail.com>
2020-04-16 13:22:18 -0400

    user can only see the questions they write

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_153209) do
+ActiveRecord::Schema.define(version: 2020_04_16_151650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_04_15_153209) do
     t.string "answer_option_one"
     t.string "answer_option_two"
     t.string "answer_option_three"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_04_15_153209) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "users"
 end
